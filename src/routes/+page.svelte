f9850c9018da479881b9cb11a192be7382747128
Abhishek Sarkar <rakrashba@outlook.com>
2022-09-25 19:07:04 +0530

    third party turn 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -172,9 +172,9 @@
 					]
 				},
 				{
-					urls: `turn:numb.viagenie.ca`,
-					username: 'webrtc@live.com',
-					credential: 'muazkh',
+					urls: `turn:192.158.29.39:3478?transport=tcp`,
+					username: '28224511:1379330808',
+					credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
 					credentialType: 'password'
 				}
 			]
